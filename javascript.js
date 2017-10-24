6dcdbfec995d4f5fa4aaecc3d37afa9716472576
katyakaterina <30529016+katyakaterina@users.noreply.github.com>
2017-10-24 13:46:19 +0300

    -

--- javascript.js
+++ javascript.js
@@ -5,3 +5,17 @@ function initMap() {
     zoom: 16
   });
 }
+function initMap() {
+    var myLatLng = {lat: 59.9388068, lng: 30.3230066};
+  
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: myLatLng
+    });
+  
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+
+    });
+  }
