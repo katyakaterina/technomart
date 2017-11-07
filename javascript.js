ca441f27377b01b1b6a232e23183d4c93ef8bf10
katyakaterina <30529016+katyakaterina@users.noreply.github.com>
2017-11-07 14:52:32 +0300

    -

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+var button = document.querySelector(".write-us");
+var modal = document.querySelector(".modal");
+var login = modal.querySelector("[name=login]");
+var email = modal.querySelector("[name=email]");
+var letter = modal.querySelector("[name=letter]");
+var form = modal.querySelector('form');
+var submit = modal.querySelector(".submit");
+var error = modal.querySelector('span');
+var storage = localStorage.getItem('login');
+var button = document.querySelector(".buy");
+var link = document.querySelector("#map");
+var modal = document.querySelector(".ctg-modal");
+var link = modal.querySelector(".check-order");
+var button = modal.querySelector(".shopping");
+
+
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -19,3 +35,55 @@ function initMap() {
 
     });
   }
+
+  button.addEventListener("click", function (e) {
+ 	 e.preventDefault();
+ 	 console.log(modal.classList.contains("open"));
+     modal.classList.toggle("open");
+
+
+
+     if(storage) {
+     	login.value = storage;
+     	email.focus();
+     } else {
+     	login.focus();
+     }
+     });
+
+    window.addEventListener("keydown", function(e) {
+    	if(e.keyCode === 27) {
+    		modal.classList.remove('open');
+    	}
+    });
+
+    window.addEventListener("keyup", function(e) {
+    	if(e.keyCode === 13) {
+    		form.addEventListener("submit", function(e){;
+    	});
+    	}
+    });
+    
+ 	form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    if(!email.value) {
+        error.classList.add("err");
+    
+    }else {
+        localStorage.setItem("Ваш email",email.value);
+        form.addEventListener("submit", function(e){;
+        });
+      
+    }
+
+    });
+    button.addEventListener("click", function(e) {
+      e.preventDefault();
+      console.log(modal.classList.contains("open"));
+    })
+
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      console.log(modal.classList.contains("open"));
+    })
